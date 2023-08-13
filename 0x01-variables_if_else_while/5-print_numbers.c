378914ba9e7f128b27a94045bb82eb0e453167db
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-08-13 04:20:04 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 success
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
