15175e2777fdbee546aaae75aae5389f7fe870ca
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-08-15 20:11:35 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - A code that prints 10 times the alphabet, in lowercase.
+ * 
+ *return: 0;
+ */
+
+void print_alphabet_x10(void)
+{
+        char ch;
+
+        int i;
+	
+        i = 0;
+
+        while (i < 10)
+        {
+                ch = 'a';
+                while (ch <= 'z')
+                {
+                        _putchar(ch);
+                        ch++;
+                }
+                _putchar('\n');
+                i++;
+        }
+}
