ca6e6ffafa36fda22170d54cb21d61dbe03b15e2
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-08-16 03:59:53 -0700

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - a function that print alphabet in lower case
+ *
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		_putchar(i);
+	_putchar('\n');
+}
