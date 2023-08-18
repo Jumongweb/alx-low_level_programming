39d1b12e30ccd49244c841972747d1c52a0cb565
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-08-18 09:50:13 -0700

    7-diagonals

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_diagonal - a function that draw a diagonal line
+ * @n: the parameter to check the last digit
+ * Return: always 0
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j <= i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
