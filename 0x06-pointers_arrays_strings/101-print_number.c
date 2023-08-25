336c1cb1037d19cf20f95f266ef5bc4510c8d4ff
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-08-24 20:06:54 -0700

    100 - 103

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - a function that print an integer
+ * @n: parameter
+ *
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	unsigned int i;
+
+	i = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		i = -n;
+	}
+
+	if (i / 10 != 0)
+	{
+		print_number(i / 10);
+	}
+	_putchar((i % 10) + '0');
+}
+
