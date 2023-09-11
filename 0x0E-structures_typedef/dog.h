e90fdfb22cd8d4bb3bb9c43caf7b51657ef43ed7
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-09-11 15:11:42 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about dog
+ * @name: identity of the dog
+ * @age: the age of the dog
+ * @owner: the person the dog belong to
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
