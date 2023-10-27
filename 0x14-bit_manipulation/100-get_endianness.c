beae26cd9f8b0e92895bb7c7c4b997016285d05e
Lawal Toheeb <lawaltoheeb36@gmail.com>
2023-10-27 13:23:20 -0700

    little indian

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,1 +1,15 @@
-ii
+#include "main.h"
+
+/**
+ * get_endianness - a function to check the system byte order
+ * Return: 0 always
+ */
+
+int get_endianness(void)
+{
+	int a = 1;
+	int little_big;
+
+	little_big = (int) (((char *)&a)[0]);
+	return (little_big);
+}
